471edb379c19f876c213e1a1632a0f6f6b84c285
Marcelo Vogt <marcelovogt43@gmail.com>
2024-02-14 01:13:25 +0100

    Added two-way searching

--- src/main.rs
+++ src/main.rs
@@ -46,6 +46,10 @@ fn main() -> Result<()> {
     let origin: &[u8] = origin.as_bytes();
     let target: &[u8] = target.as_bytes();
 
+    solve_ladder(origin, target)
+}
+
+fn solve_ladder(origin: &[u8], target: &[u8]) -> Result<()> {
     let t0 = Instant::now();
 
     let words: Vec<&[u8]> = DICTIONARY
@@ -56,73 +60,173 @@ fn main() -> Result<()> {
         .filter(|x| x.len() == origin.len())
         .collect();
 
-    let mut seen = Vec::new();
-    let mut indexes = Vec::new();
     let mut dist = 1;
-    let mut search = vec![(origin, usize::MAX)];
-    let mut next_search = vec![];
+
+    let mut seen_front = vec![origin];
+    let mut parents_front = vec![usize::MAX];
+    let mut search_front = vec![(origin, 0)];
+    let mut next_search_front = vec![];
+
+    let mut seen_back = vec![target];
+    let mut parents_back = vec![usize::MAX];
+    let mut search_back = vec![(target, 0)];
+    let mut next_search_back = vec![];
 
     loop {
         println!("Searching depth {dist}");
 
-        for &(word, index) in search.iter() {
-            let next_index = seen.len();
-            seen.push(word);
-            indexes.push(index);
+        let sol_front = iterate(
+            &mut seen_front,
+            &mut parents_front,
+            &mut search_front,
+            &mut next_search_front,
+            &words,
+            &seen_back,
+        );
 
-            for &next in words.iter() {
-                if distance(word, next) == 1
-                    && !seen.contains(&next)
-                    && search.iter().find(|(w, _)| w == &next).is_none()
-                {
-                    next_search.push((next, next_index));
+        if let Some(index_front) = sol_front {
+            let dur = t0.elapsed();
 
-                    if next == target {
-                        let dur = t0.elapsed();
+            show_solution(
+                index_front,
+                true,
+                &seen_front,
+                &parents_front,
+                &seen_back,
+                &parents_back,
+            );
 
-                        show_solution(dist, next, next_index, seen, indexes);
+            println!("\nElapsed: {dur:?}");
 
-                        println!("\nElapsed: {dur:?}");
-
-                        return Ok(());
-                    }
-                }
-            }
+            return Ok(());
         }
 
-        search.clear();
-        (search, next_search) = (next_search, search);
+        let sol_back = iterate(
+            &mut seen_back,
+            &mut parents_back,
+            &mut search_back,
+            &mut next_search_back,
+            &words,
+            &seen_front,
+        );
+
+        if let Some(index_back) = sol_back {
+            let dur = t0.elapsed();
+
+            show_solution(
+                index_back,
+                false,
+                &seen_front,
+                &parents_front,
+                &seen_back,
+                &parents_back,
+            );
+
+            println!("\nElapsed: {dur:?}");
+            return Ok(());
+        }
 
         dist += 1;
     }
 }
 
+fn iterate<'a>(
+    seen: &mut Vec<&'a [u8]>,
+    parents: &mut Vec<usize>,
+    search: &mut Vec<(&'a [u8], usize)>,
+    next_search: &mut Vec<(&'a [u8], usize)>,
+    words: &[&'a [u8]],
+    seen_other: &[&'a [u8]],
+) -> Option<usize> {
+    for &(word, parent_index) in search.iter() {
+        for &next in words.iter() {
+            if distance(word, next) == 1 && !seen.contains(&next) {
+                parents.push(parent_index);
+                next_search.push((next, seen.len()));
+                seen.push(next);
+
+                if seen_other.contains(&next) {
+                    return Some(seen.len() - 1);
+                }
+            }
+        }
+    }
+
+    std::mem::swap(search, next_search);
+
+    None
+}
+
 fn show_solution(
-    dist: usize,
-    next: &[u8],
-    next_index: usize,
-    seen: Vec<&[u8]>,
-    indexes: Vec<usize>,
+    index: usize,
+    front: bool,
+    seen_front: &[&[u8]],
+    parents_front: &[usize],
+    seen_back: &[&[u8]],
+    parents_back: &[usize],
 ) {
+    let mut solution_front = Vec::new();
+    let mut solution_back = Vec::new();
+
+    if front {
+        fill_solution(
+            &mut solution_front, //
+            index,
+            seen_front,
+            parents_front,
+        );
+        fill_solution(
+            &mut solution_back, //
+            seen_back
+                .iter()
+                .position(|&w| w == seen_front[index])
+                .unwrap(),
+            seen_back,
+            parents_back,
+        );
+    } else {
+        fill_solution(
+            &mut solution_back, //
+            index,
+            seen_back,
+            parents_back,
+        );
+        fill_solution(
+            &mut solution_front, //
+            seen_front
+                .iter()
+                .position(|&w| w == seen_back[index])
+                .unwrap(),
+            seen_front,
+            parents_front,
+        );
+    }
+
     println!(
-        "Found {:?} with distance of {dist}\n",
-        String::from_utf8_lossy(next)
+        "\nFound a {}-step solution\n",
+        solution_front.len() + solution_back.len() - 2
     );
 
-    let mut solution = Vec::new();
+    for w in solution_front
+        .iter()
+        .chain(solution_back.iter().rev().skip(1))
+    {
+        println!("{}", String::from_utf8_lossy(w));
+    }
+}
 
-    solution.push(next);
-
-    let mut i = next_index;
+fn fill_solution<'a>(
+    solution: &mut Vec<&'a [u8]>,
+    index: usize,
+    seen: &[&'a [u8]],
+    parents: &[usize],
+) {
+    let mut i = index;
 
     while i != usize::MAX {
         solution.push(seen[i]);
-        i = indexes[i];
+        i = parents[i];
     }
 
     solution.reverse();
-
-    for w in solution {
-        println!("{}", String::from_utf8_lossy(w));
-    }
 }
