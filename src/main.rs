61e07870b02b0c5d2ee2d201d5cf015cfab098da
Marcelo Vogt <marcelovogt43@gmail.com>
2024-02-14 18:50:12 +0100

    Added largest ladder

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+#![warn(clippy::all, clippy::pedantic)]
+
 mod solver;
 
 use crate::solver::solve_ladder;
 use anyhow::{anyhow, Result};
 use clap::Parser;
+use solver::find_largest_ladder;
 use std::{io::BufRead, time::Instant};
 
 #[derive(Parser, Debug)]
@@ -12,6 +15,8 @@ struct Args {
     word2: Option<String>,
     #[arg(short, long)]
     random: bool,
+    #[arg(short, long)]
+    largest: Option<usize>,
 }
 
 fn main() -> Result<()> {
@@ -20,47 +25,63 @@ fn main() -> Result<()> {
     let mut origin;
     let mut target;
 
-    if args.word1.is_none() || args.word2.is_none() {
-        origin = String::new();
-        target = String::new();
+    if let Some(length) = args.largest {
+        let t0 = Instant::now();
 
-        let mut handle = std::io::stdin().lock();
+        let result = find_largest_ladder(length, args.random);
 
-        println!("Write the origin word: ");
-        handle.read_line(&mut origin)?;
+        let dur = t0.elapsed();
 
-        println!("Write the target word: ");
-        handle.read_line(&mut target)?;
+        println!("{}-step solution found\n", result.len() - 1);
 
-        drop(handle);
+        for word in result {
+            println!("{}", String::from_utf8_lossy(word));
+        }
+
+        println!("\nElapsed {dur:?}");
     } else {
-        origin = args.word1.unwrap();
-        target = args.word2.unwrap();
+        if args.word1.is_none() || args.word2.is_none() {
+            origin = String::new();
+            target = String::new();
+
+            let mut handle = std::io::stdin().lock();
+
+            println!("Write the origin word: ");
+            handle.read_line(&mut origin)?;
+
+            println!("Write the target word: ");
+            handle.read_line(&mut target)?;
+
+            drop(handle);
+        } else {
+            origin = args.word1.unwrap();
+            target = args.word2.unwrap();
+        }
+
+        if origin.len() != target.len() {
+            return Err(anyhow!("Both words must be of the same length"));
+        }
+
+        origin.make_ascii_uppercase();
+        target.make_ascii_uppercase();
+
+        let origin: &[u8] = origin.trim().as_bytes();
+        let target: &[u8] = target.trim().as_bytes();
+
+        let t0 = Instant::now();
+
+        let result = solve_ladder(origin, target, args.random)?;
+
+        let dur = t0.elapsed();
+
+        println!("{}-step solution found\n", result.len() - 1);
+
+        for word in result {
+            println!("{}", String::from_utf8_lossy(word));
+        }
+
+        println!("\nElapsed {dur:?}");
     }
-
-    if origin.len() != target.len() {
-        return Err(anyhow!("Both words must be of the same length"));
-    }
-
-    origin.make_ascii_uppercase();
-    target.make_ascii_uppercase();
-
-    let origin: &[u8] = origin.trim().as_bytes();
-    let target: &[u8] = target.trim().as_bytes();
-
-    let t0 = Instant::now();
-
-    let result = solve_ladder(origin, target, args.random)?;
-
-    let dur = t0.elapsed();
-
-    println!("{}-step solution found\n", result.len() - 1);
-
-    for word in result {
-        println!("{}", String::from_utf8_lossy(word));
-    }
-
-    println!("\nElapsed {dur:?}");
 
     Ok(())
 }
